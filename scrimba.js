b19755349bd20508be50cb321b1d5e52c6344621
MerlynMShelley <46276713+MerlynMShelley@users.noreply.github.com>
2020-01-26 21:10:39 +0530

    Add files via upload

---
+++ scrimba.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "ReactDOM"
+
+function MyInfo(){
+	return(
+			<div>
+			<h1>Merly M Shelley</h1>
+			<p>I am a Tech Enthusiast!!</p>
+			<ul>
+				<li>New York</li>
+				<li>Washington</li>
+				<li>California</li>
+			</ul>	
+			</div>
+		)
+}
+ReactDOM.render(<MyInfo />,document.getElementById("root"))
